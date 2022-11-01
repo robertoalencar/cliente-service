5ddc8f7ea6152807c70bd638c65f6fc92f4f93d4
Roberto Alencar <rroberto.alencar@gmai.com>
2022-11-01 17:36:53 -0300

    adicionando os arquivos do projeto

---
+++ src/test/java/br/com/ifpe/clienteservice/ClienteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.clienteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
